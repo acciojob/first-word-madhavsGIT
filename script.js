4536d4739d392f2f06086a692160a4a6d84ae641
madhav <163291389+madhavsGIT@users.noreply.github.com>
2024-06-12 13:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+if(s.length === 0) return s;
+
+	let str = s.trim();
+ 	let words = str.split(" ");
+	return words[0];
+	
 }
 
 // Do not change the code below
